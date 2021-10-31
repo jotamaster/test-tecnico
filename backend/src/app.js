d162941b41aa6ea75a85c60495a3f3f0bb7f4d69
jotamaster <j3anhernandez@gmail.com>
2021-10-31 17:48:17 -0300

    backend: add cors configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import config from './config';
+import cors from 'cors';
 
 import CarsRoutes from './routes/cars.routes';
 import SeederRoutes from './routes/seeder.routes';
+
+
+const corsOptions = {
+    origin: '*', 
+    optionsSuccessStatus: 200
+};
 
 
 const app = express();
@@ -12,10 +19,11 @@ const app = express();
 app.set('port', config.appPort || 3000);
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
 //routes
 app.get('/',(req, res )=>{
-    res.json({'message':'welcome autitos api'})
+    res.json({'message':'welcome autitos api'});
 });
 
 app.use('/api/cars',CarsRoutes);
